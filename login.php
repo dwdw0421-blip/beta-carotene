9a67d72543e4fecf42a7eb2392fcd20119e5d087
Ryunosuke Umesaki <r.umesaki1995@gmail.com>
2026-04-13 16:45:33 +0900

    クラス名を変更

--- login.php
+++ login.php
@@ -11,7 +11,7 @@ include('head.php');
     ?>
 
     <main class="user-login d-flex flex-column justify-content-center">
-        <div class="user-wrapper user-login_card px-4 py-5 shadow">
+        <div class="user-wrapper user-card px-4 py-5 shadow">
             <h2 class="mb-5 text-center fw-bold">キャリコン予約</h2>
             <form action="check_login.php" method="post">
                 <div class="mb-4">
