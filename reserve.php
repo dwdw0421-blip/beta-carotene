cbf6d016e9ba3c3286982de1da6bcdab6e36ccfc
fuku <fuku12maturika2000@gmail.com>
2026-04-13 11:56:35 +0900

    footerとreserveを編集しました

---
+++ reserve.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <!-- <header></header> -->
+    <main>
+        <h1>面談予約</h1>
+
+        <div class="reserve-card">
+            <?php if () :?><p>必須</p>
+                <?php else: ?>
+                    <p>任意</p>
+                    <?php endif; ?>
+                    
+            <label>面談希望日<input type="text" class="memo" placeholder="2">2000年 1月 4日</label>
+        </div>
+    </main>
+    <footer></footer>
+</body>
+
+</html>
