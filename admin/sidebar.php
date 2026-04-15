227299aba46ed9cde39a6496f1b9ee59514d1ffe
Erika Yokota <rin0000ki@gmail.com>
2026-04-15 12:24:25 +0900

    DBから表示するように変更

--- admin/sidebar.php
+++ admin/sidebar.php
@@ -1,3 +1,14 @@
+<?php
+//コース情報を取得
+$sql = "SELECT start_date,m_classrooms.name as classroom_name FROM m_courses INNER JOIN m_classrooms ON m_courses.id = m_classrooms.course_id  ORDER BY start_date ASC";
+
+
+$stmt = $db->prepare($sql);
+$stmt->execute();
+$course_list = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+?>
+
 <aside class="admin-sidebar-wrapper">
     <p>ログイン中のユーザー：--TODO:ここにセッションに保存されている名前を表示--</p>
     <nav>
@@ -10,8 +21,13 @@
             <p>コース管理</p>
             <ul>
                 <!-- TODO:コースはDBからforeach表示する -->
-                <li>6A（2025年11月開講）</li>
-                <li>6A（2025年11月開講）</li>
+                <?php
+                foreach ($course_list as $course):
+                ?>
+                    <li><?php echo h($course['classroom_name']) ?>(<?php echo h(format_date($course['start_date'], 2)) ?>開講)</li>
+                    <!-- <li>6A（2025年11月開講）</li> -->
+
+                <?php endforeach; ?>
             </ul>
             <a href="course_add.php">コースを追加</a>
         </div>
