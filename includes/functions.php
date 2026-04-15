59e165f26ac8e8a619d136ba244898d37ed9621b
Takahisa Nagata <nagata.takahisa19@gmail.com>
2026-04-15 11:23:30 +0900

    fix

--- includes/functions.php
+++ includes/functions.php
@@ -6,11 +6,11 @@ function db_connect()
 {
     try {
         $dsn = 'mysql:dbname=' . DB_NAME . ';host=' . DB_HOST . ';charset=utf8mb4';
-        $pdo = new PDO($dsn, DB_USER, DB_PASS);
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+        $db = new PDO($dsn, DB_USER, DB_PASS);
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
         $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-        return $pdo;
+        return $db;
     } catch (PDOException $e) {
         exit('DB接続エラー: ' . $e->getMessage());
     }
