171d76a4243cc383b6b60bbd5291b35d50f4ff71
Takahisa Nagata <nagata.takahisa19@gmail.com>
2026-04-16 17:16:29 +0900

    学生一覧の時間表示を微調整

--- includes/functions.php
+++ includes/functions.php
@@ -33,6 +33,7 @@ function format_date($datetime, $type)
         1 => 'Y年m月d日 H:i:s',
         2 => 'Y年m月',
         3 => 'm月d日',
+        4 => 'Y年m月d日',
     ];
     return date($format_types[$type], strtotime($datetime));
 }
