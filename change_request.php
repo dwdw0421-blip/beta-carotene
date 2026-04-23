07c09b4123fb94e03a907c64694ba620261217cd
Takahisa Nagata <nagata.takahisa19@gmail.com>
2026-04-23 11:55:31 +0900

    確認画面から処理へつなぐ部分を修正

--- change_request.php
+++ change_request.php
@@ -16,14 +16,43 @@ $is_request_type = isset($_POST['request_type']) ? (int)$_POST['request_type'] :
 if ($is_request_detail === 1) {
     $id = isset($_POST['currentDetailId_detail']) ? $_POST['currentDetailId_detail'] : "";
     $change_detail_id = isset($_POST['changeDetailData']) ? (int)$_POST['changeDetailData'] : "";
-} else if ($is_request_type === 1) {
+} else if ($is_request_meeting_type === 1) {
     $id = isset($_POST['currentDetailId_type']) ? $_POST['currentDetailId_type'] : "";
-    $change_type = isset($_POST['radioType']) ? (int)$_POST['radioType'] : "";
+    $change_meeting_type = isset($_POST['radioType']) ? (int)$_POST['radioType'] : "";
 } else {
     exit('エラー');
 }
 
 try {
+    if ($is_request_meeting_type === 1) {
+        // 形式変更
+        //学生の予約情報を取得
+        $sql = 'SELECT 
+                    carcon_lines.date,
+                    carcon_reservation_details.slot_index,
+                    carcon_reservation_details.is_plus_carcon,
+                    CONCAT(m_students.last_name , " " , m_students.first_name) AS student_name
+                FROM carcon_reservation_details 
+                INNER JOIN carcon_reservations ON carcon_reservation_details.id = carcon_reservations.carcon_reservation_detail_id 
+                INNER JOIN carcon_lines ON carcon_lines.id = carcon_reservations.carcon_line_id
+                INNER JOIN m_meeting_types ON carcon_reservation_details.meeting_type = m_meeting_types.id
+                INNER JOIN m_students ON carcon_reservation_details.student_id = m_students.id
+                WHERE carcon_reservation_details.id = :id
+                ';
+        $stmt = $db->prepare($sql);
+        $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+        // SQLの実行
+        $stmt->execute();
+        $reservation_result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        echo "<pre>";
+        var_dump($id);
+        var_dump($reservation_result);
+        echo "</pre>";
+    } elseif ($is_request_detail === 1) {
+        // 日時変更
+    }
+
     //学生情報を取得
     $sql = 'SELECT 
     m_students.*,
@@ -39,24 +68,24 @@ try {
 
 
     //学生の予約情報を取得
-    $sql = 'SELECT 
-    carcon_reservation_details.student_id as student_id,
-    carcon_reservation_details.meeting_type as meeting_type_id,
-    m_meeting_types.name as meeting_type_name,
-    carcon_reservation_details.slot_index as slot_index,
-    carcon_reservation_details.is_plus_carcon as is_plus_carcon,
-    carcon_lines.date as date
-    FROM carcon_reservation_details 
-    INNER JOIN carcon_reservations ON carcon_reservation_details.id = carcon_reservations.carcon_reservation_detail_id 
-    INNER JOIN carcon_lines ON carcon_lines.id = carcon_reservations.carcon_line_id
-    INNER JOIN m_meeting_types ON carcon_reservation_details.meeting_type = m_meeting_types.id
-    WHERE carcon_reservation_details.student_id = :student_id
-    ORDER BY carcon_lines.date';
-    $stmt = $db->prepare($sql);
-    $stmt->bindParam(':student_id', $login_id, PDO::PARAM_INT);
-    // SQLの実行
-    $stmt->execute();
-    $reservation_result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    // $sql = 'SELECT 
+    // carcon_reservation_details.student_id as student_id,
+    // carcon_reservation_details.meeting_type as meeting_type_id,
+    // m_meeting_types.name as meeting_type_name,
+    // carcon_reservation_details.slot_index as slot_index,
+    // carcon_reservation_details.is_plus_carcon as is_plus_carcon,
+    // carcon_lines.date as date
+    // FROM carcon_reservation_details 
+    // INNER JOIN carcon_reservations ON carcon_reservation_details.id = carcon_reservations.carcon_reservation_detail_id 
+    // INNER JOIN carcon_lines ON carcon_lines.id = carcon_reservations.carcon_line_id
+    // INNER JOIN m_meeting_types ON carcon_reservation_details.meeting_type = m_meeting_types.id
+    // WHERE carcon_reservation_details.student_id = :student_id
+    // ORDER BY carcon_lines.date';
+    // $stmt = $db->prepare($sql);
+    // $stmt->bindParam(':student_id', $login_id, PDO::PARAM_INT);
+    // // SQLの実行
+    // $stmt->execute();
+    // $reservation_result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
     //学生の変更・キャンセル申請情報を取得
     $sql = 'SELECT 
@@ -103,14 +132,14 @@ try {
                 変更内容確認
             </h2>
 
-            <?php if ($is_request_type === 1): ?>
+            <?php if ($is_request_meeting_type === 1): ?>
                 <!-- 面談形式の変更 -->
                 <div
                     class="user-card px-4 py-4 shadow mb-4 rounded-4 m-auto d-flex flex-column align-items-center"
                     style="max-width: 500px;">
                     <?php
                     //必須キャリコンがtrue
-                    echo $reservation_result[0]['is_plus_carcon'] === 0
+                    echo $reservation_result['is_plus_carcon'] === 0
                         ? '<h3 class="mb-4 fw-bold">キャリコン（必須面談）</h3>'
                         : ' <h3 class="titele-carconplus mb-4 fw-bold">キャリコン＋（任意面談）</h3>';
                     ?>
@@ -118,7 +147,7 @@ try {
                         内容を確認し、チェックを入れてください。
                     </p>
 
-                    <form action="request_do.php" method="POST">
+                    <form action="change_do.php" method="POST">
                         <dl>
                             <div class="mb-4">
                                 <dt class="user-card_subtitle mb-2 fs-6 d-flex align-items-center gap-3">
@@ -126,7 +155,7 @@ try {
                                     <label class="form-check-label" for="check_date">予約日時</label>
                                 </dt>
                                 <dd class="fw-bold fs-5 ps-4">
-                                    2026年 4月 18日
+                                    <?php echo h(format_date($reservation_result["date"], 4)); ?>
                                 </dd>
                             </div>
 
@@ -136,7 +165,7 @@ try {
                                     <label class="form-check-label" for="check_time">予約時間</label>
                                 </dt>
                                 <dd class="fw-bold fs-5 ps-4">
-                                    13:00～14:00
+                                    <?php echo h(get_slot_time_by_index($reservation_result["slot_index"])); ?>
                                 </dd>
                             </div>
 
@@ -159,6 +188,8 @@ try {
                                 <input class="form-check-input mt-0" type="checkbox" id="check_type" required>
                             </div>
 
+                            <input type="hidden" name="id" value="<?php echo $id; ?>">
+                            <input type="hidden" name="change_meeting_type" value="<?php echo $change_meeting_type; ?>">
                             <button type="submit" class="btn btn-primary py-2 m-0">
                                 変更申請を送信
                             </button>
@@ -175,7 +206,7 @@ try {
                         内容を確認し、チェックを入れてください。
                     </p>
 
-                    <form action="request_do.php" method="POST">
+                    <form action="change_do.php" method="POST">
                         <dl>
                             <div class="mb-4">
                                 <dt class="user-card_subtitle mb-2 fs-6 d-flex align-items-center gap-3">
